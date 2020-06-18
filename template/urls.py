10eeaaf3d0dca8f483ca5587d89d0dd5e618e70d
AKnuazev <2536040@gmail.com>
2020-06-18 16:05:50 +0300

    Removed redundant files

--- template/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from template.views import echo, filters, extend
-
-urlpatterns = [
-    url(r'^echo/$', echo),
-    url(r'^filters/$', filters),
-    url(r'^extend/$', extend),
-]
